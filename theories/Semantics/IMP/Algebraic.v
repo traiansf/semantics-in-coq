fd98d32cb143ade55af5482ad48f2de8d5951f24
Traian Florin Serbanuta <traian.serbanuta@runtimeverification.com>
2024-03-15 12:46:22 +0200

    Finished algebraic

--- theories/Semantics/IMP/Algebraic.v
+++ theories/Semantics/IMP/Algebraic.v
@@ -1,4 +1,5 @@
 From stdpp Require Import prelude.
+From Coq Require Import FunctionalExtensionality.
 From sets Require Import Ensemble.
 From Semantics Require Import Syntax State Denotational.
 
@@ -176,8 +177,11 @@ match sigma with
 | is_while => is_C
 end.
 
-Definition zero_fn (a : fin 0) : imp_sort :=
+Definition zero_fn {T : Type} (a : fin 0) : T :=
     fin_0_inv _ a.
+
+Definition zero_dep_fn {T : fin 0 -> Type} : forall (a : fin 0), T a :=
+    fin_0_inv _.
 
 Definition if_arity : {n : nat & fin n -> imp_sort} :=
     existT 3 (fun i =>
@@ -268,7 +272,7 @@ Section sec_term_interp.
 Context
     (A : algebra IMP_sign).
 
-Fixpoint term_interp_a (a : AExp) : support A is_A :=
+Fixpoint IMP_term_interp_a (a : AExp) : support A is_A :=
 match a with
 | AVal n => op_interp A (is_int n) (fin_0_inv _)
 | Var x => op_interp A (is_var x) (fin_0_inv _)
@@ -276,26 +280,26 @@ match a with
     op_interp A is_plus
         (fun i =>
             match i with
-            | 0%fin => term_interp_a a1
-            | _ => term_interp_a a2
+            | 0%fin => IMP_term_interp_a a1
+            | _ => IMP_term_interp_a a2
             end)
 | Minus a1 a2 =>
     op_interp A is_minus
         (fun i =>
             match i with
-            | 0%fin => term_interp_a a1
-            | _ => term_interp_a a2
+            | 0%fin => IMP_term_interp_a a1
+            | _ => IMP_term_interp_a a2
             end)
 | Mul a1 a2 =>
     op_interp A is_mul
         (fun i =>
             match i with
-            | 0%fin => term_interp_a a1
-            | _ => term_interp_a a2
+            | 0%fin => IMP_term_interp_a a1
+            | _ => IMP_term_interp_a a2
             end)
 end.
 
-Fixpoint term_interp_b (b : BExp) : support A is_B :=
+Fixpoint IMP_term_interp_b (b : BExp) : support A is_B :=
 match b with
 | BVal true => op_interp A is_true (fin_0_inv _)
 | BVal false => op_interp A is_false (fin_0_inv _)
@@ -303,72 +307,276 @@ match b with
     op_interp A is_eq
         (fun i =>
             match i with
-            | 0%fin => term_interp_a a1
-            | _ => term_interp_a a2
+            | 0%fin => IMP_term_interp_a a1
+            | _ => IMP_term_interp_a a2
             end)
 | ALe a1 a2 =>
     op_interp A is_le
         (fun i =>
             match i with
-            | 0%fin => term_interp_a a1
-            | _ => term_interp_a a2
+            | 0%fin => IMP_term_interp_a a1
+            | _ => IMP_term_interp_a a2
             end)
-| Not b => op_interp A is_not (const (term_interp_b b))
+| Not b => op_interp A is_not (const (IMP_term_interp_b b))
 | And b1 b2 =>
     op_interp A is_and
         (fun i =>
             match i with
-            | 0%fin => term_interp_b b1
-            | _ => term_interp_b b2
+            | 0%fin => IMP_term_interp_b b1
+            | _ => IMP_term_interp_b b2
             end)
 | Or b1 b2 =>
     op_interp A is_or
         (fun i =>
             match i with
-            | 0%fin => term_interp_b b1
-            | _ => term_interp_b b2
+            | 0%fin => IMP_term_interp_b b1
+            | _ => IMP_term_interp_b b2
             end)
 end.
 
-Definition term_interp_if
-    (b : support A is_B) (c1 c2 : support A is_C) : support A is_C.
+
+Definition support_star_if (b : support A is_B) (c1 c2 : support A is_C) :
+    support_star A (projT2 (@arity IMP_sign is_if)).
 Proof.
-    eapply (op_interp A is_if).
-    intro i; cbn in *.
-    destruct i as [| []] eqn: Hi.
-    - exact b.
-    - exact c1.
-    - exact c2.
+  intro i; inv_fin i; [exact b |].
+  intro i; inv_fin i; [exact c1 |].
+  intro i; inv_fin i; [exact c2 |].
+  intro i; inv_fin i.
 Defined.
 
-Definition term_interp_while
-    (b : support A is_B) (c : support A is_C) : support A is_C.
+Definition support_star_while (b : support A is_B) (c : support A is_C) :
+    support_star A (projT2 (@arity IMP_sign is_while)).
 Proof.
-    eapply (op_interp A is_while).
-    intro i; cbn in *.
-    destruct i.
-    - exact b.
-    - exact c.
+  intro i; inv_fin i; [exact b |].
+  intro i; inv_fin i; [exact c |].
+  intro i; inv_fin i.
 Defined.
 
-Fixpoint term_interp_c (c : Cmd) : support A is_C :=
+Fixpoint IMP_term_interp_c (c : Cmd) : support A is_C :=
 match c with
 | Skip => op_interp A is_skip (fin_0_inv _)
-| Asgn x a => op_interp A (is_asgn x) (const (term_interp_a a))
+| Asgn x a => op_interp A (is_asgn x) (const (IMP_term_interp_a a))
 | Seq c1 c2 =>
     op_interp A is_seq
         (fun i =>
             match i with
-            | 0%fin => term_interp_c c1
-            | _ => term_interp_c c2
+            | 0%fin => IMP_term_interp_c c1
+            | _ => IMP_term_interp_c c2
             end)
 | If b c1 c2 =>
-    term_interp_if (term_interp_b b) (term_interp_c c1) (term_interp_c c2)
+    op_interp A is_if (support_star_if (IMP_term_interp_b b) (IMP_term_interp_c c1) (IMP_term_interp_c c2))
 | While b c =>
-    term_interp_while (term_interp_b b) (term_interp_c c)
+    op_interp A is_while (support_star_while (IMP_term_interp_b b) (IMP_term_interp_c c))
 end.
 
+Definition IMP_term_interp : algebra_fn IMP_term_algebra A :=
+fun s : sort IMP_sign =>
+match s as i return (support IMP_term_algebra i → support A i) with
+| is_A => IMP_term_interp_a
+| is_B => IMP_term_interp_b
+| is_C => IMP_term_interp_c
+end.
+
+#[export] Instance IMP_term_interp_morphism : AlgebraMorphism IMP_term_interp.
+Proof.
+  intros [] astar; cbn in *.
+  - by f_equal; extensionality i; inv_fin i.
+  - by f_equal; extensionality i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - by f_equal; extensionality i; inv_fin i.
+  - by f_equal; extensionality i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    by intro i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - by f_equal; extensionality i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    by intro i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    intro i; inv_fin i; [done |].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - f_equal. extensionality i; inv_fin i; [done | ].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+Qed.
+
 End sec_term_interp.
+
+Definition a_1 (a1 : AExp) : forall i : fin 1, support IMP_term_algebra (const is_A i).
+Proof.
+  intro i; inv_fin i; [exact a1 |].
+  intro i; inv_fin i.
+Defined.
+
+Definition a_2 (a1 a2 : AExp) : forall i : fin 2, support IMP_term_algebra (const is_A i).
+Proof.
+  intro i; inv_fin i; [exact a1 |].
+  intro i; inv_fin i; [exact a2 |].
+  intro i; inv_fin i.
+Defined.
+
+Definition b_1 (b : BExp) : forall i : fin 1, support IMP_term_algebra (const is_B i).
+Proof.
+  intro i; inv_fin i; [exact b |].
+  intro i; inv_fin i.
+Defined.
+
+Definition b_2 (b1 b2 : BExp) : forall i : fin 2, support IMP_term_algebra (const is_B i).
+Proof.
+  intro i; inv_fin i; [exact b1 |].
+  intro i; inv_fin i; [exact b2 |].
+  intro i; inv_fin i.
+Defined.
+
+Definition c_2 (c1 c2 : Cmd) : forall i : fin 2, support IMP_term_algebra (const is_C i).
+Proof.
+  intro i; inv_fin i; [exact c1 |].
+  intro i; inv_fin i; [exact c2 |].
+  intro i; inv_fin i.
+Defined.
+
+Definition term_support_star_if (b : BExp) (c1 c2 : Cmd) :
+    support_star IMP_term_algebra (projT2 (@arity IMP_sign is_if)).
+Proof.
+  intro i; inv_fin i; [exact b |].
+  intro i; inv_fin i; [exact c1 |].
+  intro i; inv_fin i; [exact c2 |].
+  intro i; inv_fin i.
+Defined.
+
+Definition term_support_star_while (b : BExp) (c : Cmd) :
+    support_star IMP_term_algebra (projT2 (@arity IMP_sign is_while)).
+Proof.
+  intro i; inv_fin i; [exact b |].
+  intro i; inv_fin i; [exact c |].
+  intro i; inv_fin i.
+Defined.
+
+Lemma IMP_term_interp_a_unique:
+    forall (A : algebra IMP_sign),
+    forall (h : algebra_fn IMP_term_algebra A), AlgebraMorphism h ->
+    IMP_term_interp_a A = h is_A.
+Proof.
+  intros * Hh.
+  extensionality a; cbn in a.
+  induction a; cbn.
+  - specialize (Hh (is_int z) zero_dep_fn); cbn in Hh; rewrite Hh.
+    by f_equal; extensionality i; inv_fin i.
+  - specialize (Hh (is_var n) zero_dep_fn); cbn in Hh; rewrite Hh.
+    by f_equal; extensionality i; inv_fin i.
+  - specialize (Hh is_plus (a_2 a1 a2)); cbn in Hh; rewrite Hh.
+    f_equal; extensionality i; inv_fin i; [done |].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - specialize (Hh is_minus (a_2 a1 a2)); cbn in Hh; rewrite Hh.
+    f_equal; extensionality i; inv_fin i; [done |].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - specialize (Hh is_mul (a_2 a1 a2)); cbn in Hh; rewrite Hh.
+    f_equal; extensionality i; inv_fin i; [done |].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+Qed.
+
+Lemma IMP_term_interp_b_unique:
+    forall (A : algebra IMP_sign),
+    forall (h : algebra_fn IMP_term_algebra A), AlgebraMorphism h ->
+    IMP_term_interp_b A = h is_B.
+Proof.
+  intros * Hh.
+  extensionality b; cbn in b.
+  induction b; cbn.
+  - destruct b; cbn.
+    + specialize (Hh is_true zero_dep_fn); cbn in Hh; rewrite Hh.
+      by f_equal; extensionality i; inv_fin i.
+    + specialize (Hh is_false zero_dep_fn); cbn in Hh; rewrite Hh.
+      by f_equal; extensionality i; inv_fin i.
+  - pose (Hrew := Hh is_eq (a_2 a a0)); cbn in Hrew; rewrite Hrew.
+    f_equal; extensionality i;
+      inv_fin i; [by erewrite IMP_term_interp_a_unique |].
+    intro i; inv_fin i; [by erewrite IMP_term_interp_a_unique |].
+    by intro i; inv_fin i.
+  - pose (Hrew := Hh is_le (a_2 a a0)); cbn in Hrew; rewrite Hrew.
+    f_equal; extensionality i;
+      inv_fin i; [by erewrite IMP_term_interp_a_unique |].
+    intro i; inv_fin i; [by erewrite IMP_term_interp_a_unique |].
+    by intro i; inv_fin i.
+  - specialize (Hh is_not (b_1 b)); cbn in Hh; rewrite Hh.
+    f_equal; extensionality i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - specialize (Hh is_and (b_2 b1 b2)); cbn in Hh; rewrite Hh.
+    f_equal; extensionality i; inv_fin i; [done |].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - specialize (Hh is_or (b_2 b1 b2)); cbn in Hh; rewrite Hh.
+    f_equal; extensionality i; inv_fin i; [done |].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+Qed.
+
+Lemma IMP_term_interp_c_unique:
+    forall (A : algebra IMP_sign),
+    forall (h : algebra_fn IMP_term_algebra A), AlgebraMorphism h ->
+    IMP_term_interp_c A = h is_C.
+Proof.
+  intros * Hh.
+  extensionality c; cbn in c.
+  induction c; cbn.
+  - specialize (Hh is_skip zero_dep_fn); cbn in Hh; rewrite Hh.
+    by f_equal; extensionality i; inv_fin i.
+  - pose (Hrew := Hh (is_asgn n) (a_1 a)); cbn in Hrew; rewrite Hrew.
+    f_equal; extensionality i;
+      inv_fin i; [by erewrite IMP_term_interp_a_unique |].
+    by intro i; inv_fin i.
+  - specialize (Hh is_seq (c_2 c1 c2)); cbn in Hh; rewrite Hh.
+    f_equal; extensionality i; inv_fin i; [done |].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - pose (Hrew := Hh is_if (term_support_star_if b c1 c2)); cbn in Hrew; rewrite Hrew.
+    f_equal; extensionality i;
+      inv_fin i; [by erewrite IMP_term_interp_b_unique |].
+    intro i; inv_fin i; [done |].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+  - pose (Hrew := Hh is_while (term_support_star_while b c)); cbn in Hrew; rewrite Hrew.
+    f_equal; extensionality i;
+      inv_fin i; [by erewrite IMP_term_interp_b_unique |].
+    intro i; inv_fin i; [done |].
+    by intro i; inv_fin i.
+Qed.
+
+#[export] Instance IMP_term_algebra_initial : Initial IMP_term_algebra.
+Proof.
+  intro A.
+  exists (IMP_term_interp A); split; [by typeclasses eauto |].
+  intros h Hh. extensionality s.
+  destruct s.
+  - by apply IMP_term_interp_a_unique.
+  - by apply IMP_term_interp_b_unique.
+  - by apply IMP_term_interp_c_unique.
+Qed.
 
 Definition IMP_denot_support (s : sort IMP_sign) : Type :=
 match s with
@@ -410,5 +618,45 @@ Definition IMP_denot_algebra : algebra IMP_sign :=
     support := IMP_denot_support;
     op_interp := IMP_denot_op_interp;
 |}.
+
+Lemma IMP_denot_algebra_a: IMP_term_interp_a IMP_denot_algebra = denota.
+Proof.
+  extensionality a; induction a.
+  - done.
+  - done.
+  - extensionality sigma; cbn.
+    by rewrite IHa1, IHa2.
+  - extensionality sigma; cbn.
+    by rewrite IHa1, IHa2.
+  - extensionality sigma; cbn.
+    by rewrite IHa1, IHa2.
+Qed.
+
+Lemma IMP_denot_algebra_b : IMP_term_interp_b IMP_denot_algebra = denotb.
+Proof.
+  extensionality b; induction b.
+  - by destruct b.
+  - by extensionality sigma; cbn; rewrite !IMP_denot_algebra_a.
+  - by extensionality sigma; cbn; rewrite !IMP_denot_algebra_a.
+  - extensionality sigma; cbn.
+    by rewrite IHb.
+  - extensionality sigma; cbn.
+    by rewrite IHb1, IHb2.
+  - extensionality sigma; cbn.
+    by rewrite IHb1, IHb2.
+Qed.
+
+Lemma IMP_denot_algebra_c : IMP_term_interp_c IMP_denot_algebra = denotc.
+Proof.
+  extensionality c; induction c.
+  - done.
+  - by extensionality sigma; cbn; rewrite !IMP_denot_algebra_a.
+  - extensionality sigma; cbn.
+    by rewrite IHc1, IHc2.
+  - extensionality sigma; cbn.
+    by rewrite IMP_denot_algebra_b, IHc1, IHc2.
+  - extensionality sigma; cbn.
+    by rewrite IMP_denot_algebra_b, IHc.
+Qed.
 
 End sec_IMP_algebra.
